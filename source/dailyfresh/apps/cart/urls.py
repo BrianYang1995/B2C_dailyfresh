55fddfc82627f9b0031dcbcc97860584bbdd4ab2
BrainYang1995 <1490426019@qq.com>
2018-10-14 16:56:14 +0800

    购物车模块完成

--- source/dailyfresh/apps/cart/urls.py
+++ source/dailyfresh/apps/cart/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
-
+from cart.views import CartAddView, Cart, CartChangeView, CartDeleteView
 
 urlpatterns = [
-
+    url(r'^add$', CartAddView.as_view(), name='add'),
+    url(r'^$', Cart.as_view(), name='show'),
+    url(r'^change$', CartChangeView.as_view(), name='change'),
+    url(r'^delete$', CartDeleteView.as_view(), name='delete'),
 ]
--- source/dailyfresh/apps/order/views.py
+++ source/dailyfresh/apps/order/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.views.generic import View
+from utils.mixin import LoginRequestMixIn
+from goods.models import GoodsSKU
+from user.models import Address
 
-# Create your views here.
+from django_redis import get_redis_connection
+
+
+class OrderPlaceView(LoginRequestMixIn, View):
+    """显示订单页"""
+    def post(self, request):
+        # 获取数据
+        user = request.user
+        goods_skus = request.POST.getlist('goods_skus')
+        if not goods_skus:
+            return redirect('cart:show')
+
+        # 收获地址
+        addrs = Address.objects.filter(user=user)
+
+        # 查询数据
+        goods_list = []
+        user_key = 'cart_%s' % user.id
+        count = 0  # 总数
+        amount_price = 0  # 商品总价
+        conn = get_redis_connection('default')
+
+        for sku_id in goods_skus:
+            goods = GoodsSKU.objects.get(id=sku_id)
+
+            # 获取商品数目，添加动态属性
+            goods_count = conn.hget(user_key, sku_id)
+            count += int(goods_count)
+            goods.goods_count = int(goods_count)
+            # 商品价格小计，添加动态属性
+            total_price = goods.price * int(goods_count)
+            goods.total_price = total_price
+            amount_price += total_price
+
+            goods_list.append(goods)
+
+        transite_price = 12
+        total_pay = amount_price + transite_price
+
+        # 上下文
+        context = {
+            'goods_list': goods_list,
+            'count': count,
+            'amount_price': amount_price,
+            'transite_price': transite_price,
+            'total_pay': total_pay,
+            'addrs': addrs
+        }
+        return render(request, 'place_order.html', context)
+
